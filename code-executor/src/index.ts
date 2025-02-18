169f72c78eb0fd3b1b19bc6a2cb7dfa84ea08b6a
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-02-18 23:16:58 +0530

    server updated

--- code-executor/src/index.ts
+++ code-executor/src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
-
+import codeRunRoute from "./routes/codeRunRoute"
 const app=express();
 app.use(cors({
     origin:"http://localhost:5173",
     methods:['GET','POST','DELET','PUT'],
     credentials:true
 }))
+
+app.get("/", codeRunRoute);
+
 
 app.listen(3000, ()=>{
     console.log("server running on port 3000");
