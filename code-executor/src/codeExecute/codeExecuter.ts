0a8689a7629e85e43b95269f84c6e93decdf04f6
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-02-19 02:28:58 +0530

    backend updated

---
+++ code-executor/src/codeExecute/codeExecuter.ts
@@ -0,0 +1,14 @@
+import {exec} from "child_process"
+import { error } from "console";
+import { stderr } from "process";
+
+const ExecuteCode= (filepath:string, language:string)=>{
+
+        try{
+            const command= `docker run --rm -v "${filepath}:/app/temp.cpp" -w /app gcc:latest sh -c "g++ temp.cpp -o temp && ./temp"`;
+
+            exec(command,stderr)
+        }catch(e){
+            console.log("server error during execute code", e);
+        }
+}
---
+++ code-executor/src/convertToCode/codeConverter.ts
@@ -0,0 +1,19 @@
+import fs from "fs";
+import path from "path";
+
+const FILE_DIR= path.join(__dirname, "../../temp");
+
+if(!fs.existsSync(FILE_DIR)){
+    fs.mkdirSync(FILE_DIR);
+}
+
+const CodeConverter= (code: string, language:string)=>{
+        const file= `temp_${Date.now()}.${language}`; 
+        const filepath= path.join(FILE_DIR, file);
+        
+        fs.writeFileSync(file,code);
+        return filepath;
+
+}
+
+export default CodeConverter ;
