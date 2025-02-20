7cd13c6bbd1887e4eaa7750effbbc3ac02f453b4
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-02-20 23:28:33 +0530

    Update codeExecuter.ts

--- code-executor/src/codeExecute/codeExecuter.ts
+++ code-executor/src/codeExecute/codeExecuter.ts
@@ -6,7 +6,7 @@ const ExecuteCode= (filepath:string, language:string)=>{
 
         try{
             const command= `docker run --rm -v "${filepath}:/app/temp.cpp" -w /app gcc:latest sh -c "g++ temp.cpp -o temp && ./temp"`;
-
+ 
             exec(command,stderr)
         }catch(e){
             console.log("server error during execute code", e);
