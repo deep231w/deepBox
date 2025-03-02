1be155f4a0e1fee7b841367957ed1fcdb643ae6e
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-02 13:12:02 +0530

    shadck buton initialized

---
+++ code-writer/src/components/runButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+const RunButton= ()=>{
+
+    return (
+        <Button variant="outline">Button</Button>
+
+    )
+}
+
+export default RunButton;
--- code-writer/src/pages/home.tsx
+++ code-writer/src/pages/home.tsx
@@ -2,11 +2,14 @@ import CodeWriter from "../components/codeWriter";
 import OutputRender from "../components/outputRender";
 import Card from "../components/card";
 import SelectLanguage from "@/components/selectLanguage";
+import RunButton from "@/components/runButton";
+
 const Home = () => {
     return (
         <div className="h-screen flex flex-col bg-gray-900 text-white p-4">
             <h1 className="text-2xl font-bold mb-4">DeepBox</h1>
             <SelectLanguage/>
+            <RunButton/>
             <div className="flex flex-grow gap-4">
                 <div className="w-1/2">
                     <Card>
