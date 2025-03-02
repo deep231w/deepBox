bd402d9dd3c4124bbcec5adb320f11de82646ed2
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-02 13:07:10 +0530

    shadck initialized

---
+++ code-writer/src/components/selectLanguage.tsx
@@ -0,0 +1,28 @@
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+
+  
+const SelectLanguage= ()=>{
+
+    return (
+        <div>
+            <Select>
+                <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="light">Light</SelectItem>
+                    <SelectItem value="dark">Dark</SelectItem>
+                    <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+            </Select>
+        </div>
+    )
+}
+
+export default SelectLanguage;
--- code-writer/src/pages/home.tsx
+++ code-writer/src/pages/home.tsx
@@ -1,12 +1,12 @@
 import CodeWriter from "../components/codeWriter";
 import OutputRender from "../components/outputRender";
 import Card from "../components/card";
-
+import SelectLanguage from "@/components/selectLanguage";
 const Home = () => {
     return (
         <div className="h-screen flex flex-col bg-gray-900 text-white p-4">
-            <h1 className="text-2xl font-bold mb-4">Code Editor</h1>
-
+            <h1 className="text-2xl font-bold mb-4">DeepBox</h1>
+            <SelectLanguage/>
             <div className="flex flex-grow gap-4">
                 <div className="w-1/2">
                     <Card>
