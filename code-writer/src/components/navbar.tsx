55d2a201d104c8ee10105bc4ce6a68bcf70ad44f
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-03 21:28:06 +0530

    ui updated

--- code-writer/src/components/navbar.tsx
+++ code-writer/src/components/navbar.tsx
@@ -3,7 +3,7 @@ import ThemeToggleBtn from "./themeToggleBtn";
 const Navbar = () => {
     return (
         <nav className="flex items-center justify-between px-6 py-1 shadow-md border-b border-white border-sm
-        bg-gradient-to-r from-pink-500 via-orange-500 to-yellow-500">
+        bg-gradient-to-r from-pink-500 via-orange-500 to-gray-500">
             <div className="text-3xl font-mono font-bold text-white bg-clip-text text-transparent 
             bg-gradient-to-r from-purple-400 to-blue-400 dark:text-gray-800 border-gray-700">
                 DeepBox
